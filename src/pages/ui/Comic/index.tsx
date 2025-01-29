e5d5e51b2e72b9f3f7a722b6cc932a5239fa00c0
ByP0 <andryhan09@yandex.com>
2025-01-29 15:27:04 +0200

    create template comic

--- src/pages/ui/Comic/index.tsx
+++ src/pages/ui/Comic/index.tsx
@@ -3,7 +3,20 @@ import styles from "./styles.module.scss"
 
 const Comic:FC = () => {
     return(
-        <div className={styles.comic_container}></div>
+        <div className={styles.comic_container}>
+            <div className={styles.comic_row}>
+                <img className={styles.comic} src="/src/assets/paralax/blok1.png"/> 
+                <img className={styles.comic} src="/src/assets/paralax/blok2.png"/>
+            </div>
+            <div className={styles.comic_row}>
+                <img className={styles.comic} src="/src/assets/paralax/blok3.png"/> 
+                <img className={styles.comic} src="/src/assets/paralax/blok4.png"/>
+            </div>
+            <div className={styles.comic_row}>
+                <img className={styles.comic} src="/src/assets/paralax/blok5.png"/> 
+                <img className={styles.comic} src="/src/assets/paralax/blok6.png"/>
+            </div>
+        </div>
     );
 };
 
